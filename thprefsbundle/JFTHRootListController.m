c13d9db8b33c921f6f4510db7b338a0cc8ef3f18
Jesper Flodin <jesper@flodin.fi>
2018-08-18 00:14:50 +0200

    Respring button in prefbundle working

--- thprefsbundle/JFTHRootListController.m
+++ thprefsbundle/JFTHRootListController.m
@@ -1,4 +1,5 @@
 #include "JFTHRootListController.h"
+#import <Cephei/HBRespringController.h>
 #import "../Log.h"
 
 @implementation JFTHRootListController
@@ -14,6 +15,20 @@
 	return nil;
 }
 
+- (void)respring:(PSSpecifier *)specifier {
+	PSTableCell *cell = [self cachedCellForSpecifier:specifier];
 
+	// disable the cell, in case it takes a moment
+	cell.cellEnabled = NO;
+/*
+	// ask for the url to be generated
+	[(PreferencesAppController *)[UIApplication sharedApplication] generateURL];
+
+	// sadly, this is stored in the preferences…
+	NSString *position = (__bridge NSString *)CFPreferencesCopyAppValue(CFSTR("kPreferencePositionKey"), kCFPreferencesCurrentApplication);
+*/
+	// call the main method
+	[HBRespringController respring];
+}
 
 @end
