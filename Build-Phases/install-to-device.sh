5722a05070736742289cdb41f4101df3b965fb53
Jesper Flodin <jesper@flodin.fi>
2018-09-18 22:12:00 +0200

    added more environment variables to build script

--- Build-Phases/install-to-device.sh
+++ Build-Phases/install-to-device.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Modify this to your device's IP address.
+
+if [[ "$SSH_CONNECTION_MODE" == "NET" ]]; then
+IP="192.168.2.95"
+PORT="22"
+elif [[ "$SSH_CONNECTION_MODE" == "USB" ]]; then
 IP="127.0.0.1"
 PORT="2222"
+fi
 
 # Verify that the build is for iOS Device and not a Simulator.
 if [[ "$NATIVE_ARCH" != "i386" && "$NATIVE_ARCH" != "x86_64" ]]; then
@@ -22,11 +28,16 @@ ldid -S $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/Frameworks/FileBrowser.framework/Fil
 
 echo "Copying to device"
 scp -P $PORT -r $BUILT_PRODUCTS_DIR/${WRAPPER_NAME} root@$IP:/Applications/
-#echo "Running uicache"
-#ssh root@$IP "uicache"
 
+if [[ "$RUN_UICACHE" == "YES"]]; then
+echo "Running uicache"
+ssh root@$IP -p $PORT "uicache"
+fi
+
+if [[ "$OPEN_APP" == "YES" ]]; then
 echo "opening app"
 ssh root@$IP -p $PORT "open fi.flodin.ToneManager"
+fi
 
 # This part just creates create an OS X notification to let you know that the process is done.
 # You can get terminal-notifier from https://github.com/alloy/terminal-notifier.
