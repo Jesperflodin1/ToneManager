dcd02c0c165b758d58781c5178ffbf8fcdeb0c34
Jesper Flodin <jesper@flodin.fi>
2018-09-19 22:38:20 +0200

    added appearance settings for popups

--- ToneManager/AppearanceManager.swift
+++ ToneManager/AppearanceManager.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SideMenu
+import PopupDialog
 
 class AppearanceManager {
     
@@ -16,6 +17,7 @@ class AppearanceManager {
         
         setupNavBar(nvc)
         setupSideMenu(nvc)
+        setupPopups()
     }
     fileprivate class func setupNavBar(_ nvc : UINavigationController) {
         UINavigationBar.appearance().setBackgroundImage(ColorPalette.navBarBackground?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), resizingMode: .stretch), for: .default)
@@ -49,5 +51,90 @@ class AppearanceManager {
         SideMenuManager.default.menuFadeStatusBar = false
         SideMenuManager.default.menuShadowOpacity = 0.8
         SideMenuManager.default.menuAnimationFadeStrength = 0
+    }
+    
+    fileprivate class func setupPopups() {
+        let dialogAppearance = PopupDialogDefaultView.appearance()
+        
+        dialogAppearance.backgroundColor      = .white
+        dialogAppearance.titleFont            = .boldSystemFont(ofSize: 14)
+        dialogAppearance.titleColor           = UIColor(white: 0.4, alpha: 1)
+        dialogAppearance.titleTextAlignment   = .center
+        dialogAppearance.messageFont          = .systemFont(ofSize: 14)
+        dialogAppearance.messageColor         = UIColor(white: 0.6, alpha: 1)
+        dialogAppearance.messageTextAlignment = .center
+        
+        let containerAppearance = PopupDialogContainerView.appearance()
+        
+        containerAppearance.backgroundColor = UIColor(red:0.23, green:0.23, blue:0.27, alpha:1.00)
+        containerAppearance.cornerRadius    = 2
+        containerAppearance.shadowEnabled   = true
+        containerAppearance.shadowColor     = .black
+        containerAppearance.shadowOpacity   = 0.6
+        containerAppearance.shadowRadius    = 20
+        containerAppearance.shadowOffset    = CGSize(width: 0, height: 8)
+//        containerAppearance.shadowPath      = CGPath(...)
+        
+        let overlayAppearance = PopupDialogOverlayView.appearance()
+        
+        overlayAppearance.color           = .black
+        overlayAppearance.blurRadius      = 20
+        overlayAppearance.blurEnabled     = true
+        overlayAppearance.liveBlurEnabled = false
+        overlayAppearance.opacity         = 0.7
+        
+        let buttonAppearance = DefaultButton.appearance()
+        
+        // Default button
+        buttonAppearance.titleFont      = .systemFont(ofSize: 14)
+        buttonAppearance.titleColor     = UIColor(red: 0.25, green: 0.53, blue: 0.91, alpha: 1)
+        buttonAppearance.buttonColor    = .clear
+        buttonAppearance.separatorColor = UIColor(white: 0.9, alpha: 1)
+        
+        // Below, only the differences are highlighted
+        
+        // Cancel button
+        CancelButton.appearance().titleColor = .lightGray
+        
+        // Destructive button
+        DestructiveButton.appearance().titleColor = .red
+    }
+    
+    fileprivate class func setPopupDarkmode() {
+        // Customize dialog appearance
+        let pv = PopupDialogDefaultView.appearance()
+        pv.titleFont    = UIFont(name: "HelveticaNeue-Light", size: 16)!
+        pv.titleColor   = .white
+        pv.messageFont  = UIFont(name: "HelveticaNeue", size: 14)!
+        pv.messageColor = UIColor(white: 0.8, alpha: 1)
+        
+        // Customize the container view appearance
+        let pcv = PopupDialogContainerView.appearance()
+        pcv.backgroundColor = UIColor(red:0.23, green:0.23, blue:0.27, alpha:1.00)
+        pcv.cornerRadius    = 2
+        pcv.shadowEnabled   = true
+        pcv.shadowColor     = .black
+        
+        // Customize overlay appearance
+        let ov = PopupDialogOverlayView.appearance()
+        ov.blurEnabled     = true
+        ov.blurRadius      = 30
+        ov.liveBlurEnabled = true
+        ov.opacity         = 0.7
+        ov.color           = .black
+        
+        // Customize default button appearance
+        let db = DefaultButton.appearance()
+        db.titleFont      = UIFont(name: "HelveticaNeue-Medium", size: 14)!
+        db.titleColor     = .white
+        db.buttonColor    = UIColor(red:0.25, green:0.25, blue:0.29, alpha:1.00)
+        db.separatorColor = UIColor(red:0.20, green:0.20, blue:0.25, alpha:1.00)
+        
+        // Customize cancel button appearance
+        let cb = CancelButton.appearance()
+        cb.titleFont      = UIFont(name: "HelveticaNeue-Medium", size: 14)!
+        cb.titleColor     = UIColor(white: 0.6, alpha: 1)
+        cb.buttonColor    = UIColor(red:0.25, green:0.25, blue:0.29, alpha:1.00)
+        cb.separatorColor = UIColor(red:0.20, green:0.20, blue:0.25, alpha:1.00)
     }
 }
