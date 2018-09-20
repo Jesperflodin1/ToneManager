b7cec185b2f1d65c53e59ec353d36894a7aff96f
Jesper Flodin <jesper@flodin.fi>
2018-09-20 09:38:30 +0200

    extended xcode build script to create deb file

--- Build-Phases/install-to-device.sh
+++ Build-Phases/install-to-device.sh
@@ -12,9 +12,7 @@ fi
 
 # Verify that the build is for iOS Device and not a Simulator.
 if [[ "$NATIVE_ARCH" != "i386" && "$NATIVE_ARCH" != "x86_64" ]]; then
-# Kill any running instances and remove the app folder.
-echo "Removing previous app"
-ssh root@$IP -p $PORT "killall ${TARGETNAME}; rm -rf /Applications/${WRAPPER_NAME}"
+
 # Self sign the build.
 echo "Signing with ldid"
 ldid -S$BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/entitlements.xml $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/$TARGETNAME
@@ -26,9 +24,61 @@ ldid -S $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/Frameworks/XLActionController.framew
 ldid -S $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/Frameworks/PopupDialog.framework/PopupDialog
 ldid -S $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/Frameworks/DynamicBlurView.framework/DynamicBlurView
 
-# Copy it over.
-
 #rm -r $BUILT_PRODUCTS_DIR/${WRAPPER_NAME}/Frameworks/libswift*
+
+# DEB
+if [[ "$CREATE_DEB" == "YES" ]]; then
+echo "Preparing deb archive"
+cd ~/Documents/Projects/Tweaks/
+rm -r BUILD
+mkdir -p BUILD/Applications
+cp -R $BUILT_PRODUCTS_DIR/${WRAPPER_NAME} BUILD/Applications/
+cp -R ToneManager/layout/DEBIAN BUILD/
+
+INFO_PLIST_PATH=BUILD/Applications/ToneManager.app/Info.plist
+version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${INFO_PLIST_PATH}")
+build=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFO_PLIST_PATH}")
+
+echo "Version: ${version}-${build}"
+echo "Version: ${version}-${build}" >> BUILD/DEBIAN/control
+
+rm -r BUILD/Applications/${WRAPPER_NAME}/Frameworks/libswift*
+rm -r BUILD/Applications/${WRAPPER_NAME}/libswift*
+find ~/Documents/Projects/Tweaks/ -name ".DS_Store" -depth -exec rm {} \;
+
+dpkg-deb -bZgzip BUILD ToneManager/
+cp ToneManager/*.deb ToneManager/packages/
+
+#install
+echo "Installing deb"
+ssh root@$IP -p $PORT "mkdir -p /var/mobile/Documents/tmpdeb"
+scp -P $PORT -r ToneManager/*.deb root@$IP:/var/mobile/Documents/tmpdeb/
+ssh root@$IP -p $PORT "dpkg -i /var/mobile/Documents/tmpdeb/*.deb"
+ssh root@$IP -p $PORT "apt-get install -f"
+ssh root@$IP -p $PORT "rm -r /var/mobile/Documents/tmpdeb"
+
+#repo
+echo "Updating cydia repo"
+cd ~/Documents/GitHub/jesperflodin1.github.io/
+cp ~/Documents/Projects/Tweaks/ToneManager/packages/*.deb debs
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fks Packages
+git add .
+git commit -m 'updated ToneManager'
+git push
+cd -
+
+rm ToneManager/*.deb
+echo "Done!"
+
+else
+
+# Kill any running instances and remove the app folder.
+echo "Removing previous app"
+ssh root@$IP -p $PORT "killall ${TARGETNAME}; rm -rf /Applications/${WRAPPER_NAME}"
+
+
+# Copy it over.
 
 echo "Copying to device"
 scp -P $PORT -r $BUILT_PRODUCTS_DIR/${WRAPPER_NAME} root@$IP:/Applications/
@@ -42,8 +92,6 @@ if [[ "$OPEN_APP" == "YES" ]]; then
 echo "opening app"
 ssh root@$IP -p $PORT "open fi.flodin.ToneManager"
 fi
+fi
 
-# This part just creates create an OS X notification to let you know that the process is done.
-# You can get terminal-notifier from https://github.com/alloy/terminal-notifier.
-# You can remove this line if you want.
 fi
