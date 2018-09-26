da24831b3ccb1cea6023d970f7162497706a5123
Jesper Flodin <jesper@flodin.fi>
2018-09-26 07:59:47 +0200

    added license text

--- ToneManager/Models/Ringtone.swift
+++ ToneManager/Models/Ringtone.swift
@@ -3,8 +3,30 @@
 //  ToneManager
 //
 //  Created by Jesper Flodin on 2018-09-07.
-//  Copyright © 2018 Jesper Flodin. All rights reserved.
 //
+//
+//  MIT License
+//
+//  Copyright (c) 2018 Jesper Flodin
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 import Foundation
 import BugfenderSDK
 import AVFoundation
@@ -13,7 +35,20 @@ import AVFoundation
 final class Ringtone : NSObject, Codable {
     
     /// Name visible in the ringtone picker
-    var name: String
+    private(set) var name: String
+    
+    func changeName(_ newName: String, ignoreInstalledStatus: Bool) {
+        if ignoreInstalledStatus {
+            self.name = newName
+            return
+        }
+        if self.installed {
+            RingtoneManager.uninstallRingtone(ringtoneObject: self, useHUD: false) {
+                NotificationCenter.default.postMainThreadNotification(notification: Notification(name: .ringtoneStoreDidReload))
+            }
+        }
+        
+    }
     
     /// Identifier used by tonelibrary
     var identifier: String?
